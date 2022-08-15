f4433092c5d4a913323bb48d22b809ab290d0b2e
Renan <eijieizo@gmail.com>
2022-08-15 16:36:18 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Olá, me chamo Isadora ")
+n = input('Qual seu nome? ')
+print('Muito prazer',n)
+print('Vammos realizar a soma entre 2 valores? ')
+v = int(input('Qual o primeiro valor: '))
+v1 = int(input('Qual o segundo valor? '))
+s = v+v1
+print('A soma dos valores {} e {} é igual {}'.format(v, v1, s))
